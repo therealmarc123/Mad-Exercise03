e480b2e5eab3c700059f0b5385d9a990504276ea
therealmarc123 <114685117+therealmarc123@users.noreply.github.com>
2024-03-17 18:53:38 +0100

    Initialize exercise-03-lecture

---
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.movieappmad24
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.movieappmad24.navigation.Navigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Navigation()
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/movieappmad24/screens/HomeScreen.kt
@@ -0,0 +1,237 @@
+package com.example.movieappmad24.screens
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Movie App")},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        bottomBar = {
+            NavigationBar {
+                NavigationBarItem(
+                    label = { Text("Home") },
+                    selected = true,
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Go to home"
+                    )}
+                )
+                NavigationBarItem(
+                    label = { Text("Watchlist") },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = "Go to watchlist"
+                    )}
+                )
+            }
+        }
+    ){ innerPadding ->
+        MovieList(
+            modifier = Modifier.padding(innerPadding),
+            movies = getMovies(),
+            navController = navController
+        )
+    }
+}
+
+
+@Composable
+fun MovieList(
+    modifier: Modifier,
+    movies: List<Movie> = getMovies(),
+    navController: NavController
+    ){
+    LazyColumn(modifier = modifier) {
+        items(movies) { movie ->
+            MovieRow(movie = movie){ movieId ->
+                navController.navigate("detailscreen/$movieId")
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(
+    movie: Movie,
+    onItemClick: (String) -> Unit = {}
+    ){
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(5.dp)
+        .clickable {
+            onItemClick(movie.id)
+        },
+        shape = ShapeDefaults.Large,
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Column {
+
+            MovieCardHeader(imageUrl = movie.images[0])
+
+            MovieDetails(modifier = Modifier.padding(12.dp), movie = movie)
+
+        }
+    }
+}
+
+@Composable
+fun MovieCardHeader(imageUrl: String) {
+    Box(
+        modifier = Modifier
+            .height(150.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        MovieImage(imageUrl)
+
+        FavoriteIcon()
+    }
+}
+
+@Composable
+fun MovieImage(imageUrl: String){
+    SubcomposeAsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(imageUrl)
+            .crossfade(true)
+            .build(),
+        contentScale = ContentScale.Crop,
+        contentDescription = "movie poster",
+        loading = {
+            CircularProgressIndicator()
+        }
+    )
+}
+
+@Composable
+fun FavoriteIcon() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        contentAlignment = Alignment.TopEnd
+    ){
+        Icon(
+            tint = MaterialTheme.colorScheme.secondary,
+            imageVector = Icons.Default.FavoriteBorder,
+            contentDescription = "Add to favorites")
+    }
+}
+
+
+@Composable
+fun MovieDetails(modifier: Modifier, movie: Movie) {
+    var showDetails by remember {
+        mutableStateOf(false)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = movie.title)
+        Icon(modifier = Modifier
+            .clickable {
+                showDetails = !showDetails
+            },
+            imageVector =
+            if (showDetails) Icons.Filled.KeyboardArrowDown
+            else Icons.Default.KeyboardArrowUp, contentDescription = "show more")
+    }
+
+
+    AnimatedVisibility(
+        visible = showDetails,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Column (modifier = modifier) {
+            Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Genre: ${movie.genre}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.bodySmall)
+            Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.bodySmall)
+
+            Divider(modifier = Modifier.padding(3.dp))
+
+            Text(buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                    append("Plot: ")
+                }
+                withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Normal)){
+                    append(movie.plot)
+                }
+            })
+        }
+    }
+}
